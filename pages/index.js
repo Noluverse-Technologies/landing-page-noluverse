0e60bcefbdcacc95eed7b8f9964de8aa627354b4
Ali Hayder <alihayder19@gmail.com>
2022-04-01 18:52:11 +0600

    Fix Favicon

--- pages/index.js
+++ pages/index.js
@@ -33,9 +33,8 @@ export default function App() {
   return (
     <>
       <Head>
-        <title>Noluverse</title>
+        <title>Noluverse Technologies</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" type="image/x-icon" href="/logo.png"></link>
       </Head>
 
       {/* for desktop------------------------- */}
